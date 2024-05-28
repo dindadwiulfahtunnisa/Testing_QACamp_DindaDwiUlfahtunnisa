4422224443033df5027dff9bac7cdc414080b8f6
dindadwiulfahtunnisa <dindadwiulfahh@gmail.com>
2024-05-28 14:14:28 +0700

    My First Commit

---
+++ Test Suites/Make Appointment.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Make Appointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>59a30abe-b48b-4f6d-afdc-5303b179b478</testSuiteGuid>
+</TestSuiteEntity>
